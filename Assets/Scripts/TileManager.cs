31b0a38f9e325311aed5157451e4e656bdbdeacd
fanalex756 <afan@chapman.edu>
2021-04-11 15:42:54 -0700

    Initial commit

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TileManager : MonoBehaviour
 {
     public Owner CurrentPlayer;
-    public Tile[] Tiles = new Tile[4];
+    public Tile[] Tiles = new Tile[9];
+    public ScoreUIManager keepScore;
+    public Button resetButton;
+    public Button quitButton;
 
     public enum Owner
     {
@@ -19,14 +23,30 @@ public class TileManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        StartGame();
+    }
+
+    public void StartGame()
+    {
         won = false;
         CurrentPlayer = Owner.Sword;
+        resetButton.transform.localScale = new Vector3(0, 0, 0);
+        quitButton.transform.localScale = new Vector3(0, 0, 0);
     }
 
     public void ChangePlayer()
     {
-        if (CheckForWinner())
+        if (CurrentPlayer == Owner.None)
             return;
+
+        if (CheckForWinner())
+        {
+            resetButton.transform.localScale = new Vector3(1, 1, 1);
+            quitButton.transform.localScale = new Vector3(1, 1, 1);
+            keepScore.UpdateScore(CurrentPlayer);
+            CurrentPlayer = Owner.None;
+            return;
+        }
 
         if (CurrentPlayer == Owner.Sword)
             CurrentPlayer = Owner.Shield;
@@ -34,17 +54,39 @@ public class TileManager : MonoBehaviour
             CurrentPlayer = Owner.Sword;
     }
 
+    public void Reset()
+    {
+        foreach (Tile tile in Tiles)
+        {
+            tile.owner = Owner.None;
+            tile.GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        StartGame();
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("You quit the game");
+    }
+
     public bool CheckForWinner()
     {
-        if (Tiles[0].owner == CurrentPlayer && Tiles[1].owner == CurrentPlayer)
+        if (Tiles[0].owner == CurrentPlayer && Tiles[1].owner == CurrentPlayer && Tiles[2].owner == CurrentPlayer)
             won = true;
-        else if (Tiles[0].owner == CurrentPlayer && Tiles[2].owner == CurrentPlayer)
+        else if (Tiles[3].owner == CurrentPlayer && Tiles[4].owner == CurrentPlayer && Tiles[5].owner == CurrentPlayer)
             won = true;
-        else if (Tiles[0].owner == CurrentPlayer && Tiles[3].owner == CurrentPlayer)
+        else if (Tiles[6].owner == CurrentPlayer && Tiles[7].owner == CurrentPlayer && Tiles[8].owner == CurrentPlayer)
             won = true;
-        else if (Tiles[1].owner == CurrentPlayer && Tiles[2].owner == CurrentPlayer)
+        else if (Tiles[0].owner == CurrentPlayer && Tiles[3].owner == CurrentPlayer && Tiles[6].owner == CurrentPlayer)
             won = true;
-        else if (Tiles[1].owner == CurrentPlayer && Tiles[3].owner == CurrentPlayer)
+        else if (Tiles[1].owner == CurrentPlayer && Tiles[4].owner == CurrentPlayer && Tiles[7].owner == CurrentPlayer)
+            won = true;
+        else if (Tiles[2].owner == CurrentPlayer && Tiles[5].owner == CurrentPlayer && Tiles[8].owner == CurrentPlayer)
+            won = true;
+        else if (Tiles[2].owner == CurrentPlayer && Tiles[4].owner == CurrentPlayer && Tiles[6].owner == CurrentPlayer)
+            won = true;
+        else if (Tiles[0].owner == CurrentPlayer && Tiles[4].owner == CurrentPlayer && Tiles[8].owner == CurrentPlayer)
             won = true;
 
         if (won)
@@ -55,4 +97,6 @@ public class TileManager : MonoBehaviour
 
         return false;
     }
+
+
 }
